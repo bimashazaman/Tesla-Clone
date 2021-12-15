410d1936fde3548d3f8683380e284bad4d2bb6a5
bimashazaman <bimashazaman1234@gmail.com>
2021-12-15 21:08:38 +0600

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <Container>
       <Section
-        tittle='Model S'
+        title='Model S'
         description='Online order for Tesla Model S.'
         backgroundImage='model-s.jpg'
         leftButtonText='Custom Order'
@@ -14,7 +14,7 @@ const Home = () => {
       />
 
       <Section
-        tittle='Model Y'
+        title='Model Y'
         description='online order for Tesla Model Y.'
         backgroundImage='model-y.jpg'
         leftButtonText='Custom Order'
@@ -22,7 +22,7 @@ const Home = () => {
       />
 
       <Section
-        tittle='Model 3'
+        title='Model 3'
         description='Online order for Tesla Model 3.'
         backgroundImage='model-3.jpg'
         leftButtonText='Custom Order'
@@ -30,9 +30,49 @@ const Home = () => {
       />
 
       <Section
-        tittle='Model X'
+        title='Model X'
         description='Online order for Tesla Model X.'
         backgroundImage='model-x.jpg'
+        leftButtonText='Custom Order'
+        rightButtonText='Existing Inventory'
+      />
+
+      <Section
+        title='solar roof'
+        description='Online order for Tesla solar roof.'
+        backgroundImage='solar-roof.jpg'
+        leftButtonText='Custom Order'
+        rightButtonText='Existing Inventory'
+      />
+
+      <Section
+        title='New Interior'
+        description='Online order for Tesla new interior.'
+        backgroundImage='new-interior.jpg'
+        leftButtonText='Custom Order'
+        rightButtonText='Existing Inventory'
+      />
+
+      <Section
+        title='Solar-Panels'
+        description='Online order for Tesla solar panels.'
+        backgroundImage='solar-panel.jpg'
+        leftButtonText='Custom Order'
+        rightButtonText='Existing Inventory'
+      />
+
+      <Section
+        title='Game'
+        description='Online order for Tesla game.'
+        backgroundImage='game.jpg'
+        leftButtonText='Custom Order'
+        rightButtonText='Existing Inventory'
+      />
+
+      <Section
+        title='Accessories'
+        description='Online order for Tesla accessories.'
+        backgroundImage='accessories.jpg'
         leftButtonText='Custom Order'
         rightButtonText='Existing Inventory'
       />
