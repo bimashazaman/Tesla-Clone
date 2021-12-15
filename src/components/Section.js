96385369ab53334bb74393ab773b1436500b7436
bimashazaman <bimashazaman1234@gmail.com>
2021-12-16 01:45:27 +0600

    initial commit

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 function Section({
   title,
@@ -10,16 +11,20 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImage}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
 
       <Buttons>
-        <ButtonGroup>
-          <Leftbutton>{leftButtonText}</Leftbutton>
-          <Rightbutton>{rightButtonText}</Rightbutton>
-        </ButtonGroup>
+        <Fade left>
+          <ButtonGroup>
+            <Leftbutton>{leftButtonText}</Leftbutton>
+            <Rightbutton>{rightButtonText}</Rightbutton>
+          </ButtonGroup>
+        </Fade>
       </Buttons>
     </Wrap>
   )
